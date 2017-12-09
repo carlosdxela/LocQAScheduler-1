3e01de18dcfce8c4a015a5838ce37ebfd74011aa
Carlos D. Lopez <carlosdxela@gmail.com>
2017-12-08 21:34:50 -0800

    app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: 'testers',
-  loadChildren: 'app/tester/tester.module#TesterModule'},
-  {path: 'projects',
-  loadChildren: 'app/project/project.module#ProjectModule'},
-  {path: 'assignments',
-  loadChildren: 'app/assignments/project.module#AssignmentsModule'}
+  loadChildren: 'app/tester/tester.module#TesterModule'}
 ];
 
 @NgModule({
